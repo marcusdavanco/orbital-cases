8516367d86c57addc50b138fd91cf45f74582a6a
Marcus <md.daves@gmail.com>
2024-04-12 09:30:12 -0300

    chore: update text siry global

--- src/app/cases/siry-global/page.tsx
+++ src/app/cases/siry-global/page.tsx
@@ -3,8 +3,9 @@
 import { Hero } from "@/components/hero";
 import { Section } from "@/components/section";
 import Image from "next/image";
+import { useState } from "react";
 
-export default function page() {
+export default function page() {  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -26,7 +27,7 @@ export default function page() {
           }
         }
         title="Contexto" 
-        content={['A Orbital Tech teve a honra de colaborar com a Siry Global em um projeto empolgante que envolveu a modernização completa de sua presença online. Como consultoria de TI especializada, focamos em desenvolvimento de software, integração de sistemas e UI/UX.', 'Nosso objetivo era elevar a identidade digital da Siry Global, especialmente para seu produto estrela, o Evolution.']}
+        content={['A Orbital Tech teve a honra de colaborar com a Siry Global em um projeto empolgante que envolveu a modernização completa de sua presença online. Como consultoria de TI especializada, focamos em desenvolvimento web e UI/UX.', 'Nosso objetivo era elevar a identidade digital da Siry Global, especialmente para seu produto estrela, o Evolution.']}
         customHeight={597}
       />
       <Section         
@@ -36,11 +37,11 @@ export default function page() {
       <Section         
         dark
         title="Tecnologia" 
-        content={['Para alcançar esses objetivos, utilizamos tecnologias de ponta no desenvolvimento do site. O novo site institucional e o hot site do Evolution foram construídos com as mais recentes ferramentas de web development e web design.', 'Implementamos técnicas de UI/UX para garantir uma experiência de usuário atraente e intuitiva. Além disso, incorporamos elementos de SEO para melhorar a visibilidade nos motores de busca e integração com redes sociais para uma presença online ampliada.']}
+        content={['Para alcançar esses objetivos, utilizamos tecnologias de ponta no desenvolvimento do site. O novo site institucional e o hotsite do Evolution foram construídos com as mais recentes ferramentas de web development e web design.', 'Implementamos técnicas de UI/UX para garantir uma experiência de usuário atraente e intuitiva. Além disso, incorporamos elementos de SEO para melhorar a visibilidade nos motores de busca e integração com redes sociais para uma presença online ampliada.']}
       />
       <Section         
         title="Soluções" 
-        content={['A solução que propusemos e implementamos foi um rebranding completo da presença online da Siry Global. Para o site institucional, criamos um design moderno e responsivo que reflete a essência da empresa de forma elegante e profissional. Já para o hot site do Evolution, desenvolvemos uma plataforma interativa, destacando os recursos e benefícios do produto de forma cativante.', 'A integração de sistemas permitiu uma experiência contínua para os usuários, do site principal ao hot site do produto.']}
+        content={['A solução que propusemos e implementamos foi um rebranding completo da presença online da Siry Global. Para o site institucional, criamos um design moderno e responsivo que reflete a essência da empresa de forma elegante e profissional. Já para o hotsite do Evolution, desenvolvemos uma plataforma interativa, destacando os recursos e benefícios do produto de forma cativante.', 'A integração de sistemas permitiu uma experiência contínua para os usuários, do site principal ao hotsite do produto.']}
       />
       <section className="h-[823px] w-full relative pb-[100px] bg-white">
       <Image 
@@ -55,7 +56,7 @@ export default function page() {
       <Section         
         dark
         title="Impacto" 
-        content={['O impacto desse projeto foi significativo para o negócio da Siry Global. A nova presença online resultou em um aumento notável na interação dos clientes com a marca. O site institucional recebeu elogios pela sua usabilidade e design, refletindo positivamente na imagem da empresa. ', 'O hot site do Evolution, por sua vez, gerou um aumento nas consultas e interesse pelo produto, resultando em um aumento nas conversões e nas vendas.']}
+        content={['O impacto desse projeto foi significativo para o negócio da Siry Global. A nova presença online resultou em um aumento notável na interação dos clientes com a marca. O site institucional recebeu elogios pela sua usabilidade e design, refletindo positivamente na imagem da empresa.', 'O hotsite do Evolution, por sua vez, gerou um aumento nas consultas e interesse pelo produto, resultando em um aumento nas conversões e nas vendas.']}
       />
     </main>
   );
