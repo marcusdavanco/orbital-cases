5ec5024f2ea8b73b46a96faaa01b79fbd850caec
Marcus <md.daves@gmail.com>
2024-04-13 08:54:10 -0300

    chore: image adjustment revert

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -34,7 +34,7 @@ export function Hero({
         <Image 
           src={imageUrl}
           fill
-          objectFit={typeof window !== "undefined" && window?.outerHeight >= 1024 ? "cover" : "fill"}
+          objectFit={"cover"}
           alt="Orbital Tech Logo"        
           quality={70}
           className={`lg:animate-fade-left animate-fade  animate-duration-[1500ms] lg:animate-ease-in -z-10`}
